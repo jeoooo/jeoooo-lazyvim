4b0ace22105f3cee12f0b55379899dbf2e95e2f7
jeoooo <jeocarlolubao@gmail.com>
2025-03-31 07:46:05 +0800

    updated discord rich presence filename to presence

--- lua/plugins/discord-presence.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "andweeb/presence.nvim",
-  event = "VeryLazy",
-  config = function()
-    vim.defer_fn(function()
-      require("presence"):setup({})
-    end, 1000) -- Wait 1 second before running setup
-  end,
-}
