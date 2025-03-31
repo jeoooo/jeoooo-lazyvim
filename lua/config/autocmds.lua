fc2d6f4cb72a2c82710df34364c84f2c9e60300d
JEO CARLO LUBAO <jeocarlolubao@gmail.com>
2025-03-31 18:33:20 +0800

    removed any related plugins that affect markdown linting and warning popups

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- disable warnings for markdown
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.diagnostic.disable(0) -- Disable diagnostics for current buffer
+  end,
+})
