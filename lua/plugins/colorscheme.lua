bad16d00e2be567b47ca325a7f6e97ba52fa76af
JEO CARLO LUBAO <jeocarlolubao@gmail.com>
2025-04-01 18:57:13 +0800

    theme updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -66,21 +66,19 @@
 -- }
 --
 
-
-
--- TODO : NEEDS WORKING ON REAL TIME SWITCHING OF THEMES INSTEAD OF PRESSING ENTER MANUALLY
+-- TODO : NEEDS FIXING FOR onedark_dark Transparency
 return {
   -- Main theme (default: "onedark_dark")
+
   {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
     config = function()
-      local current_transparency = true
       local current_theme = "onedark_dark"
 
       local function apply_theme()
-        -- If the theme is "onelight", force transparency OFF
-        local transparency = (current_theme ~= "onelight") and current_transparency or false
+        -- Set transparency based on theme
+        local transparency = (current_theme == "onedark_dark")
 
         require("onedarkpro").setup({
           options = {
@@ -90,45 +88,16 @@ return {
           },
         })
         vim.cmd("colorscheme " .. current_theme)
-        print("Theme: " .. current_theme .. " | Transparency: " .. (transparency and "On" or "Off"))
+        -- print("Theme: " .. current_theme .. " | Transparency: " .. (transparency and "On" or "Off"))
       end
-
-      -- 🌟 Theme Picker (`<leader>uC`)
-      vim.keymap.set("n", "<leader>uC", function()
-        local themes = {
-          "onedark_dark", "onelight", "onedark_vivid", -- Onedark variants
-          "aurora", "monokai-pro", "dracula", "material",
-          "tokyonight", "vim-cyberpunk", "2077", "matrix"
-        }
-
-        vim.ui.select(themes, { prompt = "Select Theme" }, function(choice)
-          if choice then
-            current_theme = choice
-            current_transparency = (choice ~= "onelight") -- Reset transparency based on theme
-            apply_theme()
-          end
-        end)
-      end, { desc = "Switch Theme" })
-
-      -- 🌟 Transparency Toggle (`<leader>uP`)
-      vim.keymap.set("n", "<leader>uP", function()
-        -- Prevent toggling transparency for "onelight"
-        if current_theme == "onelight" then
-          print("Transparency is always OFF for 'onelight'")
-          return
-        end
-
-        current_transparency = not current_transparency
-        apply_theme()
-      end, { desc = "Toggle Transparency" })
 
       -- Apply default theme
       apply_theme()
     end,
   },
-
   -- Additional themes
   { "ray-x/aurora" },
+  { "navarasu/onedark.nvim" },
   { "loctvl842/monokai-pro.nvim" },
   { "Mofiqul/dracula.nvim" },
   { "marko-cerovac/material.nvim" },
@@ -139,6 +108,3 @@ return {
 }
 
 --
-
-
-
